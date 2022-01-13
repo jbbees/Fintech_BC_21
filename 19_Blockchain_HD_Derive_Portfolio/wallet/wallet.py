402460ce81bb9450904f25e20946d2b059c95778
John Kelly <jakelly4@gmail.com>
2022-01-12 22:58:25 -0500

    Updated code

--- 19_Blockchain_HD_Derive_Portfolio/wallet/wallet.py
+++ 19_Blockchain_HD_Derive_Portfolio/wallet/wallet.py
@@ -1,26 +1,33 @@
 # Import dependencies
 import subprocess
 import json
+import os
 from dotenv import load_dotenv
 
 # Load and set environment variables
-load_dotenv()
-mnemonic=os.getenv("mnemonic")
+load_dotenv('keys.env')
+mnemonic = os.getenv("mnemonic")
 
 # Import constants.py and necessary functions from bit and web3
 # YOUR CODE HERE
  
+from constants.py import *
  
 # Create a function called `derive_wallets`
 def derive_wallets(# YOUR CODE HERE):
-    command = # YOUR CODE HERE
+    
+    # YOUR CODE HERE 
+    command = ''
+
     p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
     output, err = p.communicate()
     p_status = p.wait()
     return json.loads(output)
 
 # Create a dictionary object called coins to store the output from `derive_wallets`.
-coins = # YOUR CODE HERE
+# YOUR CODE HERE
+
+coins = {}
 
 # Create a function called `priv_key_to_account` that converts privkey strings to account objects.
 def priv_key_to_account(# YOUR CODE HERE):
